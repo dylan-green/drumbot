8bf250248dc1edeae4437e4b4cb15d4229f67194
Dylan Green <dylan.green61@gmail.com>
2019-07-01 03:22:41 +1000

    formatting

--- react-starter/src/DrumMachine.js
+++ react-starter/src/DrumMachine.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './DrumMachine.css';
 import { fetch } from 'whatwg-fetch';
-import AudioEngine, {browserSupportsWebAudio } from './AudioEngine';
-const apiHost = process.env.REACT_APP_API_HOST || 'https://api.noopschallenge.com';
+import AudioEngine, { browserSupportsWebAudio } from './AudioEngine';
+const apiHost =
+  process.env.REACT_APP_API_HOST || 'https://api.noopschallenge.com';
 
 export default class DrumMachine extends React.Component {
   state = {
@@ -20,107 +21,117 @@ export default class DrumMachine extends React.Component {
     if (!browserSupportsWebAudio()) {
       return this.setState({
         loading: false,
-        error: 'This browser does not support WebAudio. Try Edge, Firefox, Chrome or Safari.'
+        error:
+          'This browser does not support WebAudio. Try Edge, Firefox, Chrome or Safari.'
       });
     }
   }
 
   powerOn = () => {
-    this.audioEngine = new AudioEngine({ onStep: this.onStep});
-    this.audioEngine.prepare().then(() =>
-      fetch(`${apiHost}/drumbot/patterns`).then(r => r.json()).then(
-        patterns => {
-          this.setState({ patterns, poweredOn: true }, () => {
-            const randomIndex = Math.floor(Math.random() * patterns.length);
-            this.selectPattern(randomIndex)
-          });
-        },
-        err => {
-          this.setState({ error: 'Oops. Something went wrong. Please check your connection and refresh your browser.', loading: false });
-        }
-    ), error => {
-      this.setState({ error: true, loading: false });
-    });
-  }
+    this.audioEngine = new AudioEngine({ onStep: this.onStep });
+    this.audioEngine.prepare().then(
+      () =>
+        fetch(`${apiHost}/drumbot/patterns`)
+          .then(r => r.json())
+          .then(
+            patterns => {
+              this.setState({ patterns, poweredOn: true }, () => {
+                const randomIndex = Math.floor(Math.random() * patterns.length);
+                this.selectPattern(randomIndex);
+              });
+            },
+            err => {
+              this.setState({
+                error:
+                  'Oops. Something went wrong. Please check your connection and refresh your browser.',
+                loading: false
+              });
+            }
+          ),
+      error => {
+        this.setState({ error: true, loading: false });
+      }
+    );
+  };
 
   startClock = () => {
     this.audioEngine.startClock(this.state.pattern.beatsPerMinute);
 
     this.setState({ playing: true });
-  }
+  };
   stopClock = () => {
     this.audioEngine.stopClock();
 
-    this.setState({ playing: false});
-  }
+    this.setState({ playing: false });
+  };
 
-  onStep = ({
-    position,
-  }) => {
+  onStep = ({ position }) => {
     this.setState({ position });
-  }
+  };
 
   selectStep = (instrument, step) => {
-    this.state.pattern.tracks.forEach((track) => {
-      if (track.instrument === instrument) { track.steps[step] = !track.steps[step]; }
+    this.state.pattern.tracks.forEach(track => {
+      if (track.instrument === instrument) {
+        track.steps[step] = !track.steps[step];
+      }
     });
     this.setState(this.state.pattern);
     this.audioEngine.setPattern(this.state.pattern);
-  }
+  };
 
   selectPattern(index) {
     if (index < 0) index = this.state.patterns.length - 1;
     if (index >= this.state.patterns.length) index = 0;
-    const pattern = this.state.patterns[index]
+    const pattern = this.state.patterns[index];
     if (this.state.playing) {
       this.stopClock();
     }
 
-
-    fetch(`${apiHost}/drumbot/patterns/${pattern.name}`).then(r => r.json()).then(
-      pattern => {
+    fetch(`${apiHost}/drumbot/patterns/${pattern.name}`)
+      .then(r => r.json())
+      .then(pattern => {
         this.setState({ pattern, patternIndex: index, loading: false });
         this.audioEngine.setPattern(pattern);
-      }
-    );
+      });
   }
 
   nextPattern = () => {
     this.selectPattern(this.state.patternIndex + 1);
-  }
+  };
 
   previousPattern = () => {
     this.selectPattern(this.state.patternIndex - 1);
-  }
+  };
 
   render() {
-
-    if (this.state.error) return (
-      <div className='DrumMachine__Error'>
-        {this.state.error}
-      </div>
-    );
+    if (this.state.error)
+      return <div className='DrumMachine__Error'>{this.state.error}</div>;
 
     if (!this.state.poweredOn) {
       return (
         <div className='DrumMachine'>
           <div className='DrumMachine__GetStarted'>
             <p>Welcome to DrumBot</p>
-            <button className='DrumMachine__StartStopButton' onClick={this.powerOn}>Start!</button>
+            <button
+              className='DrumMachine__StartStopButton'
+              onClick={this.powerOn}>
+              Start!
+            </button>
           </div>
         </div>
       );
     }
 
-    const { pattern: { tracks, name }, position: { step } } = this.state;
+    const {
+      pattern: { tracks, name },
+      position: { step }
+    } = this.state;
 
     return (
       <div className='DrumMachine'>
         <div className='DrumMachine__TopPanel'>
-          <div className="DrumMachine__Logo">
-            <div className="DrumMachine__Title">
-              CR-78
-            </div>
+          <div className='DrumMachine__Logo'>
+            <div className='DrumMachine__Title'>CR-78</div>
           </div>
           {this.state.poweredOn && (
             <>
@@ -128,16 +139,24 @@ export default class DrumMachine extends React.Component {
                 <div className='DrumMachine__PatternButton'>
                   <button onClick={this.previousPattern}>&lt;</button>
                 </div>
-                <div className='DrumMachine__SelectedPattern'>
-                  {name}
-                </div>
+                <div className='DrumMachine__SelectedPattern'>{name}</div>
                 <div className='DrumMachine__PatternButton'>
                   <button onClick={this.nextPattern}>&gt;</button>
                 </div>
               </div>
               <div className='DrumMachine__Transport'>
-                <button disabled={this.state.playing} className='DrumMachine__StartStopButton' onClick={this.startClock}>Start</button>
-                <button disabled={!this.state.playing} className='DrumMachine__StartStopButton' onClick={this.stopClock}>Stop</button>
+                <button
+                  disabled={this.state.playing}
+                  className='DrumMachine__StartStopButton'
+                  onClick={this.startClock}>
+                  Start
+                </button>
+                <button
+                  disabled={!this.state.playing}
+                  className='DrumMachine__StartStopButton'
+                  onClick={this.stopClock}>
+                  Stop
+                </button>
               </div>
             </>
           )}
@@ -149,16 +168,20 @@ export default class DrumMachine extends React.Component {
               <div className='DrumMachine__TrackLabel'>{track.instrument}</div>
               <div className='DrumMachine__TrackSteps'>
                 {track.steps.map((trackStep, i) => (
-                  <div 
-                    onClick={() => {this.selectStep(track.instrument, i)}}
-                    className={`DrumMachine__Step DrumMachine__Step--${step === i ? 'Active' : 'Inactive'} DrumMachine__Step--${trackStep ? 'On' : 'Off'}`} key={i}>
-                  </div>
+                  <div
+                    onClick={() => {
+                      this.selectStep(track.instrument, i);
+                    }}
+                    className={`DrumMachine__Step DrumMachine__Step--${
+                      step === i ? 'Active' : 'Inactive'
+                    } DrumMachine__Step--${trackStep ? 'On' : 'Off'}`}
+                    key={i}
+                  />
                 ))}
               </div>
             </div>
           ))}
         </div>
-
       </div>
     );
   }
--- react-starter/src/SoundPlayer.js
+++ react-starter/src/SoundPlayer.js
@@ -1,24 +1,16 @@
 import SamplePlayer from './SamplePlayer';
 
-const samples = [
-  'clap',
-  'cowbell',
-  'kick',
-  'snare',
-  'hihat',
-  'ride',
-  'rim'
- ];
+const samples = ['clap', 'cowbell', 'kick', 'snare', 'hihat', 'ride', 'rim'];
 
 export default class SoundPlayer {
   constructor() {
     this.instruments = {};
   }
 
-  prepare = (context) => {
+  prepare = context => {
     // load all samples
-    return Promise.all(samples.map(sample => this.loadSample(context, sample)))
-  }
+    return Promise.all(samples.map(sample => this.loadSample(context, sample)));
+  };
 
   loadSample = (context, instrument) => {
     const url = `./samples/${instrument}.wav`;
@@ -29,29 +21,29 @@ export default class SoundPlayer {
 
       request.responseType = 'arraybuffer';
 
-      request.onload =  () => {
+      request.onload = () => {
         var audioData = request.response;
 
-        context.decodeAudioData(audioData, (buffer) => {
-          this.instruments[instrument] = new SamplePlayer(buffer);
-          resolve();
-        }, reject);
+        context.decodeAudioData(
+          audioData,
+          buffer => {
+            this.instruments[instrument] = new SamplePlayer(buffer);
+            resolve();
+          },
+          reject
+        );
       };
 
       request.send();
     });
-  }
+  };
 
-  play({
-    context,
-    instrument,
-    timing
-  }) {
+  play({ context, instrument, timing }) {
     const player = this.instruments[instrument];
     const gainNode = context.createGain();
     gainNode.gain.setValueAtTime(0.1, timing);
     gainNode.connect(context.destination);
 
-    if (player) player.play({context, timing, destination: gainNode});
+    if (player) player.play({ context, timing, destination: gainNode });
   }
 }
